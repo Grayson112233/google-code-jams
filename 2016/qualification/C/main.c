82d54a82a82e6dd1fad49161209bfc9bf1b73c78
Grayson Pike <graysonpike@gmail.com>
2016-12-20 16:37:25 -0600

    WIP: Brute force sucks

--- 2016/qualification/C/main.c
+++ 2016/qualification/C/main.c
@@ -19,26 +19,89 @@ int nontrival_factor(int x) {
     return -1;
 }
 
-bool validate_coin(int coin) {
-    for(int i = 2; i <= 10; i++) {
-        int temp = coin;
-        int value = 0;
-        int j = 0;
-        while(temp != 0) {
-            if(temp % 10 == 1) {
-                value += (int)pow(i, j);
-            }
-            temp /= 10;
-            j++;
+int get_value(bool *coin, int len, int base) {
+    int value = 0;
+    for(int i = 0; i < len; i++) {
+        if(coin[i]) {
+            value += (int)pow(base, i);
         }
+    }
+    return value;
+}
+
+bool validate_coin(bool *coin, int len) {
+    if(!coin[0] || !coin[len-1]) {
+        return false;
+    }
+    // For each base
+    for(int base = 2; base <= 10; base++) {
+        int value = get_value(coin, len, base);
         if(nontrival_factor(value) == -1) {
-            printf("Fails, %d is prime (base %d)\n", value, i);
+            //printf("Fails, %d is prime (base %d)\n", value, base);
             return false;
         }
     }
     return true;
 }
 
+// Assigns int to coin array (ignoring index 0 and len-1)
+void int_to_coin(int n, bool *coin, int len) {
+    int chunk; // Variable to store pow() calculation to prevent recalculation
+    // For each position in the binary number
+    for(int i = len-3; i >= 0; i--) {
+        chunk = (int)pow(2, i);
+        if(n >= chunk) {
+            coin[len-2-i] = true;
+            n -= chunk;
+        } else {
+            coin[len-2-i] = false;
+        }
+    }
+}
+
+void print_coin(bool *coin, int len) {
+    for(int i = 0; i < len; i++) {
+        printf("%c", coin[i] ? '1' : '0');
+    }
+}
+
+void print_proof(bool *coin, int len) {
+    // For each base
+    for(int base = 2; base <= 10; base++) {
+        int value = get_value(coin, len, base);
+        printf(" %d", nontrival_factor(value));
+    }
+}
+
 int main() {
-    printf("%d: %s\n", 100101, validate_coin(100101) ? "true" : "false");
+
+    int len, n;
+    FILE *fp = fopen("small.in", "r");
+    fscanf(fp, "%*d"); // Always one case, ignore
+    fscanf(fp, "%d %d", &len, &n);
+
+    printf("len is %d\n", len);
+    bool coin[len];
+    
+    // Valid coins have 1's at index 0 and len-1 always
+    coin[0] = true;
+    coin[len-1] = true;
+
+    printf("staring coin is: ");
+    print_coin(coin, len);
+    printf("\n");
+
+    int i = 0;
+    int num_found = 0;
+    while(num_found < n) {
+        int_to_coin(i, coin, len);
+        //print_coin(coin, len);
+        if(validate_coin(coin, len)) {
+            print_coin(coin, len);
+            print_proof(coin, len);
+            printf("\n");
+            num_found++;
+        }
+        i++;
+    }
 }
